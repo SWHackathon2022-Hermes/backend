baf9f2699282c3b9f38487d89ebab585c21309a5
yujinni <86652449+yujin37@users.noreply.github.com>
2022-06-24 08:14:21 +0900

    db연결

--- Reportview_bot/restapi.py
+++ Reportview_bot/restapi.py
@@ -2,42 +2,57 @@ from pickletools import read_string1
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_restful import reqparse
+from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 api = Api(app)
+
+#mysql 연결
+db = pymysql.connect(host='192.168.219.142', port = 3306, user='계정명',
+db='hermes', password='password', charset='utf8')
+            
 class CreateUser(Resource):
     def post(self):
-        try:#보내는 코드
+        try:
+            #입력 파라미터를 설정
             parser = reqparse.RequestParser()
-            parser.add_argument('tent_1', type=str)
-            parser.add_argument('tent_2', type=str)
-            parser.add_argument('tent_3', type=str)
-            parser.add_argument('tent_4', type=str)
-            args = parser.parse_args()
-            #받는 코드
-            _tent_1 = args['tent_1']
-            _tent_2 = args['tent_2']
-            _tent_3 = args['tent_3']
-            _tent_4 = args['tent_4']
-            
-            f=open("./testing.txt",'w')
-    
-            content={'tent_1': args['tent_1'], 'tent_2': args['tent_2'],
-                     'tent_3': args['tent_3'], 'tent_4': args['tent_4']}
-            f.write(content['tent_1']+'\n')
-            f.write(content['tent_2']+'\n')
-            f.write(content['tent_3']+'\n')
-            f.write(content['tent_4']+'\n')
-            
-            f.close()
-            
-            return {'StatusCode': '200', 'Message': '저장 성공!'}    
+            #필수 매개변수
+            parser.add_argument('db_Category', type=str)
+            parser.add_argument('Address', type=str)
+            parser.add_argument('AddressDetail', type=str)
+            parser.add_argument('Description', type=str)
+
+            #입력 파싱
+            args = parser.parse_args() 
+            _db_Category = args['db_Category']
+            _Address = args['Address']
+            _AddressDetail = args['AddressDetail']
+            _Description = args['Description']
+
+            curs = db.cursor()
+
+            #sql문 실행
+            sql = "insert into report_table (db_Category, Address, AddressDetail, Description) values(%s,%s,%s,%s)"
+            val = (_db_Category,_Address,_AddressDetail, _Description)
+            curs.execute(sql,val)
+
+            #db 커밋
+            db.commit()
+            #db 닫기
+            db.close()
+
+            #성공값 출력
+            return {'StatusCode': '200', 'Message': '저장 성공!'}
+
+        #에러메세지 출력
         except Exception as e:
             return {'error': str(e)}
 
 api.add_resource(CreateUser, '/report')
 
+#flask ip, port 설정
 if __name__ ==  '__main__':
     app.run(
-    host="localhost",
-    port=8080)
+    host='0.0.0.0',
+    port=50)
